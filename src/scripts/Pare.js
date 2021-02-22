a11d2057dd0e69b68b92c97fe443217cbf36ff47
martacamacho <martacamachovargas@gmail.com>
2021-02-23 00:15:09 +0100

    deleted node modules not necessary and change operator on conversion

--- src/scripts/Pare.js
+++ src/scripts/Pare.js
@@ -16,7 +16,7 @@ export default {
             if(number == isNaN || number =="") {
                 return  this.msg = `Introduce un número válido`
             } else {
-                return  this.msg = `El cambio de ${number}€ en dólares son ${(number / 1.23).toFixed(2)}$`
+                return  this.msg = `El cambio de ${number}€ en dólares son ${(number * 1.23).toFixed(2)}$`
             }
         },
         theAlert: function() {
